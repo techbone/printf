70fc793b1d862e31a02d632a792acdc12b6e638c
Ucheadaeze <ucheigbokwe25@gmail.com>
2022-10-18 02:10:20 -0700

    Printf Task

--- main.h
+++ main.h
@@ -74,21 +74,21 @@ char buffer[], int flags, char flag_ch, int width, int precision, int size);
 int print_non_printable(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 
-/* Funcion to print memory address */
+/* Function to print memory address */
 int print_pointer(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 
-/* Funciotns to handle other specifiers */
+/* Functions to handle other specifiers */
 int get_flags(const char *format, int *i);
 int get_width(const char *format, int *i, va_list list);
 int get_precision(const char *format, int *i, va_list list);
 int get_size(const char *format, int *i);
 
-/*Function to print string in reverse*/
+/* Function to print string in reverse */
 int print_reverse(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 
-/*Function to print a string in rot 13*/
+/* Function to print a string in rot 13 */
 int print_rot13string(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 
