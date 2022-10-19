03d7cc3e2bdb06f894a871920a6eb9a3d20dd20d
Musa Muhammad Etudaye <musaawwaletudaye@gmail.com>
2022-10-19 03:26:36 -0700

    tasks

--- utils.c
+++ utils.c
@@ -24,7 +24,7 @@ int append_hexa_code(char ascii_code, char buffer[], int i)
 {
 	char map_to[] = "0123456789ABCDEF";
 	/* The hexa format code is always 2 digits long */
-	(ascii_code < 0)
+	(ascii_code < 0);
 		ascii_code *= -1;
 
 	buffer[i++] = '\\';
